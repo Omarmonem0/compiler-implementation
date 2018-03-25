757545ff61e8ae1919a0e02beb852abf6f50fd00
tarekalqaddy <tarek.alqaddy@gmail.com>
2018-03-25 03:18:50 +0200

    Remove deepcopy from Thomson & fix subset bug

--- Thompson.py
+++ Thompson.py
@@ -11,7 +11,12 @@ class State:
         }
 
     def __str__(self):
-        return self.data['name']
+        # return 'State#{} trans({}): {}'.format(self.data['name'], len(self.data['trans'].keys()), self.data['trans'])
+        return str(self.data['name'])
+
+    def __repr__(self):
+        # return 'State#{} trans({}): {}'.format(self.data['name'], len(self.data['trans'].keys()), self.data['trans'])
+        return str(self.data['name'])
 
 
 class Nfa:
@@ -27,26 +32,31 @@ class Nfa:
             self.make_character_nfa(character)
 
     def __str__(self):
-        return self.states
+        # return 'NFA states({}): {}'.format(len(self.states), self.states)
+        return self.name
+
+    def __repr__(self):
+        # return 'NFA states({}): {}'.format(len(self.states), self.states)
+        return self.name
 
     def make_character_nfa(self, character):
-
         Nfa.count += 1
         state_one = State(Nfa.count)
         Nfa.count += 1
         state_two = State(Nfa.count)
-        state_one.data['trans'] = {
-            character: state_two
-        }
+        state_one.data['trans'][character] = state_two
         self.start_state = state_one
         self.final_states.append(state_two)
         self.states.append(state_one)
         self.states.append(state_two)
 
     @staticmethod
+    def get_name():
+        Nfa.count += 1
+        return Nfa.count
+
+    @staticmethod
     def union(first_nfa, second_nfa):
-        copy_one = deepcopy(first_nfa)
-        copy_two = deepcopy(second_nfa)
         combined_nfa = Nfa(None)
         Nfa.count += 1
         new_start_state = State(Nfa.count)
@@ -56,11 +66,11 @@ class Nfa:
             #  the new start state to the old start states of nfa1 and nfa2
             'E': [first_nfa.start_state,  second_nfa.start_state]
         }
-        for final_state in copy_one.final_states:
+        for final_state in first_nfa.final_states:
             final_state.data['trans'] = {  # epsilon transition from final states to new the final state
                 'E': new_end_state
             }
-        for final_state in copy_two.final_states:
+        for final_state in second_nfa.final_states:
             final_state.data['trans'] = {
                 'E': new_end_state
             }
@@ -68,40 +78,38 @@ class Nfa:
         combined_nfa.final_states.append(new_end_state)  # assigning new end state
         combined_nfa.states.append(new_start_state)  # append start and end state to states list of new  the nfa
         combined_nfa.states.append(new_end_state)
-        for first_nfa_state in copy_one.states:  # append states of first nfa to states list of the new nfa
+        for first_nfa_state in first_nfa.states:  # append states of first nfa to states list of the new nfa
             combined_nfa.states.append(first_nfa_state)
-        for second_nfa_state in copy_two.states:  # append states of second nfa to states list of the new nfa
+        for second_nfa_state in second_nfa.states:  # append states of second nfa to states list of the new nfa
             combined_nfa.states.append(second_nfa_state)
         return combined_nfa
 
     @staticmethod
     def concat(first_nfa, second_nfa):
-        copy = deepcopy(first_nfa)
         combined_nfa = Nfa(None)
-        combined_nfa.start_state = copy.start_state
-        for first_nfa_final_state in copy.final_states:  # append states of first nfa to states list of the new nfa
+        combined_nfa.start_state = first_nfa.start_state
+        for first_nfa_final_state in first_nfa.final_states:  # append states of first nfa to states list of the new nfa
             first_nfa_final_state.data['trans'] = second_nfa.start_state.data['trans']
         for second_nfa_final_state in second_nfa.final_states:
             combined_nfa.final_states.append(second_nfa_final_state)
             combined_nfa.states.append(second_nfa_final_state)
-        for first_nfa_state in copy.states:  # append states of first nfa to states list of the new nfa
+        for first_nfa_state in first_nfa.states:  # append states of first nfa to states list of the new nfa
             combined_nfa.states.append(first_nfa_state)
         return combined_nfa
 
     @staticmethod
     def klein(nfa):  # star
         new_nfa = Nfa(None)
-        copy = deepcopy(nfa)
         Nfa.count += 1
         new_start_state = State(Nfa.count)
         Nfa.count += 1
         new_final_state = State(Nfa.count)
         new_start_state.data['trans'] = {
-            'E': [new_final_state, copy.start_state]
+            'E': [new_final_state, nfa.start_state]
         }
-        for final_states in copy.final_states:
-            final_states.data['trans'].update({'E': [copy.start_state, new_final_state]})
-        for nfa_state in copy.states:  # append states of first nfa to states list of the new nfa
+        for final_states in nfa.final_states:
+            final_states.data['trans'].update({'E': [nfa.start_state, new_final_state]})
+        for nfa_state in nfa.states:  # append states of first nfa to states list of the new nfa
             new_nfa.states.append(nfa_state)
         new_nfa.start_state = new_start_state
         new_nfa.final_states.append(new_final_state)
