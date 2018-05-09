68cc040c973d6616e0927b456841edc812c87fda
tarekalqaddy <tarek.alqaddy@gmail.com>
2018-05-10 01:50:13 +0200

    Add main classes in phase 2.

---
+++ phase 2: Parser/NonTerminal.py
@@ -0,0 +1,7 @@
+from Node import Node
+
+
+class NonTerminal(Node):
+    def __init__(self, value, next):
+        super(value, next)
+        self.type = 'N'
---
+++ phase 2: Parser/Terminal.py
@@ -0,0 +1,7 @@
+from Node import Node
+
+
+class Terminal(Node):
+    def __init__(self, value, next):
+        super(value, next)
+        self.type = 'T'
