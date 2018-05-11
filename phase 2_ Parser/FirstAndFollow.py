bb2cb4c495ebc396994c722bef674f2f8ec69490
omarmonem0 <omarahmedmonem@gmail.com>
2018-05-10 19:39:58 -0700

    follow implementation

--- phase 2_ Parser/FirstAndFollow.py
+++ phase 2_ Parser/FirstAndFollow.py
@@ -13,76 +13,126 @@ def gettype(node):
 
 def get_first(productions):
     first = {
-        'E': [],
         'S': [],
+        'A': [],
+        'E': [],
         'T': [],
-        'M': [],
-        'F': []
     }
     for key in productions:
         for node in productions[key]:
             if gettype(node) == 0:
-                first[key].append(node.value)
+                first[key].append(node)
             elif gettype(node) == 1:
                 temp = first_for_node(node, productions)
-                first[key].append(temp.value)
+                for node in temp:
+                    first[key].append(node)
     return first
 
 
 def first_for_node(node, productions):
+    temporary_first = []
     for node in productions[node.value]:
         if gettype(node) == 0:
-            return node
+            temporary_first.append(node)
+
         else:
             return first_for_node(node, productions)
+    return temporary_first
+
+def get_follow(productions, first):
+    follow = {
+        'S': [],
+        'A': [],
+        'E': [],
+        'T': [],
+    }
+    buffer = {}
+    nodes_have_epsilons = []
+    for key in productions:
+        for node in productions[key]:
+            if check_type(node) == 3:
+                if gettype(node.next.next) == 0:
+                    if node.next.next.value not in follow[node.next.value]:
+                        follow[node.next.value].append(node.next.next.value)
+                        continue
+                    else:
+                        continue
+                for element in first[node.next.next.value]:
+                    if element.value not in follow[node.next.value]:
+                        follow[node.next.value].append(element.value)
+            if check_type(node) == 2:
+                if not follow[node.next.value]:
+                    buffer.update({node.next.value: key})
+                    continue
+
+    for key in buffer:
+        for element in follow[buffer[key]]:
+            follow[key].append(element)
+    return follow
 
 
-def get_follow():
-    return 0
-
+def check_type(node):
+    count = 0
+    count += 1
+    node = node.next
+    while node is not None:
+            node = node.next
+            count += 1
+    return count
 
 # Non Terminals
 
+
+S = NonTerminal('S', None)
+A = NonTerminal('A', None)  # e bar
 E = NonTerminal('E', None)
-S = NonTerminal('S', None)  # e bar
-T = NonTerminal('T', None)
-M = NonTerminal('M', None)  # t bar
-F = NonTerminal('F', None)
+T = NonTerminal('T', None)  # t bar
+
 # Terminals
-plus = Terminal('+', None)
-Id = Terminal('id', None)
-star = Terminal('*', None)
+AND = Terminal('&', None)
+a = Terminal('a', None)
+b = Terminal('b', None)
+c = Terminal('c', None)
 openBracket = Terminal('(', None)
 closeBracket = Terminal(')', None)
 
 # making rules
 n_epsilon = Terminal('$', None)
-n1 = copy.deepcopy(T)
-n1.next = S
 
-T_duplicate = copy.deepcopy(T)
-T_duplicate.next = copy.deepcopy(S)
-n2 = copy.deepcopy(plus)
-n2.next = T_duplicate
+n1 = copy.deepcopy(openBracket)
+A_duplicate = copy.deepcopy(A)
+A_duplicate.next = closeBracket
+n1.next = A_duplicate
 
-n3 = copy.deepcopy(F)
-n3.next = M
+n2 = copy.deepcopy(T)
+n2.next = copy.deepcopy(E)
 
-F_duplicate = copy.deepcopy(F)
-F_duplicate.next = copy.deepcopy(M)
-n4 = copy.deepcopy(star)
-n4.next = F_duplicate
 
-E_duplicate = copy.deepcopy(E)
-n5 = copy.deepcopy(openBracket)
-n5.next = E_duplicate
-E_duplicate.next = closeBracket
+n3 = copy.deepcopy(AND)
+T_duplicate_two = copy.deepcopy(T)
+T_duplicate_two.next = copy.deepcopy(E)
+n3.next = T_duplicate_two
+
+
+n4 = copy.deepcopy(openBracket)
+A_duplicate_two = copy.deepcopy(A)
+A_duplicate_two.next = closeBracket
+n4.next = A_duplicate
+
+n5 = a
+n6 = b
+n7 = c
 
 rules = {
-    'E': [n1],
-    'S': [n2, n_epsilon],
-    'T': [n3],
-    'M': [n4, n_epsilon],
-    'F': [n5]
+    'S': [n1],
+    'A': [n2],
+    'E': [n3],
+    'T': [n4, n5, n6, n7]
 }
-print(get_first(rules))
+first = get_first(rules)
+print("first")
+for key in first:
+    for nodes in first[key]:
+        print(key, '->', nodes.value)
+print("follow")
+print(get_follow(rules, first))
